34cdc071d2128f75ff545b051bc335826f604eaa
tobi <onatadetobi212@gmail.com>
2025-08-07 10:38:10 +0100

    v2 added changeable prefix

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import secrets
 
 app = FastAPI()
 
+class KeyRequest(BaseModel):
+    prefix: str = 'sk_test'
+
 @app.post('/generate-key')
-def generate_key():
-    key = 't-key_'  + secrets.token_urlsafe(24)
+def generate_key(data: KeyRequest):
+    key = f"{data.prefix}_{secrets.token_urlsafe(24)}"
     return {'apikey': key}
