2806022f28b7ec6a1d41d088a0a36eb1e01e2f4c
tobi <onatadetobi212@gmail.com>
2025-08-07 10:51:56 +0100

    v3 added customizable length

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import secrets
 
 app = FastAPI()
 
 class KeyRequest(BaseModel):
-    prefix: str = 'sk_test'
+    prefix: str = 'sk_test'                           #Prefix of the API KEY
+    length: int = Field(default=32, ge=8, le=64)      #Length of the API KEY
 
 @app.post('/generate-key')
 def generate_key(data: KeyRequest):
-    key = f"{data.prefix}_{secrets.token_urlsafe(24)}"
+
+    random_part = secrets.token_urlsafe(data.length)[:data.length]
+    key = f"{data.prefix}_{random_part}"
     return {'apikey': key}
